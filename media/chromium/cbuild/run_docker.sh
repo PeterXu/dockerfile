6808f65ebbe0c42ab28dd7b4fda3ee33c627b15d
yongze <yongze@meetdeMac-mini.local>
2020-08-07 13:29:35 +0800

    refine cbuild

--- media/chromium/cbuild/run_docker.sh
+++ media/chromium/cbuild/run_docker.sh
@@ -1,4 +1,4 @@
 docker run --name chromium_build --network default \
-    -v ~/Workspace/webrtc-android:/webrtc-android \
-	-v ~/Workspace/depot_tools_android:/depot_tools \
-	-it -d lark.io/cbuild
+    -v ~/Workspace:/Workspace \
+    -v ~/.depot_tools2:/depot_tools \
+    -it -d lark.io/cbuild
